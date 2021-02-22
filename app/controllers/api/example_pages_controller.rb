0a87d4d773a2e1af51b293c22485d6aaa63f8a28
Sam Tanner <Samuel.m.s.tanner@gmail.com>
2021-02-22 12:30:44 -0800

    adds .jb view for time

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,5 +1,16 @@
 class Api::ExamplePagesController < ApplicationController
   def hello_action
-    render json: { message: "hello" }
+    @message = "hello",
+    @time = Time.now.strftime("%a, %r %Y")
+
+    render "hello.json.jb"
+  end
+
+  def multiple_messages
+    render json: [{ f_name: "sam", l_name: "tanner", b_day: "June 29th" }, { f_name: "tess", l_name: "levin", b_day: "June 19th" }]
+  end
+
+  def html_example
+    render inline: '<h1> hello world </h1> <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" />'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/hello_path" => "example_pages#hello_action"
+    get "/multiple_messages_path" => "example_pages#multiple_messages"
+    get "/html_code" => "example_pages#html_example"
   end
 end
